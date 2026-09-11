4cc22343328e24a7398986e63328a40ef39c6909
Alen <gameralen2002@gmail.com>
2026-09-11 18:17:28 +0200

    Agregado SignalR

---
+++ F1Manager/Hubs/CarreraHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace F1Manager.Hubs;
+
+public class CarreraHub : Hub
+{
+    public async Task EnviarMensaje(string mensaje)
+    {
+        await Clients.All.SendAsync("RecibirMensaje", mensaje);
+    }
+}
