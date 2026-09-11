cea9ff0846e66d2116839eb0dd31679667329b27
Alen <gameralen2002@gmail.com>
2026-09-12 00:42:25 +0200

    Vuelta rapida , mala vuelta , vuelta excepcional añadidos

--- F1Manager.GameEngine/Carrera.cs
+++ F1Manager.GameEngine/Carrera.cs
@@ -5,4 +5,9 @@ public class Carrera
     public int NumeroVueltas { get; set; }
 
     public List<Piloto> Pilotos { get; set; } = new();
+    public int VueltaActual { get; set; }
+
+    public string PilotoVueltaRapida { get; set; } = "";
+
+    public double? TiempoVueltaRapida { get; set; }
 }
